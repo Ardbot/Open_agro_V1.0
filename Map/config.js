6cfa385f67bea432a4dcdf9a2f7cb79e4b0e3af1
Ardbot <45563576+Ardbot@users.noreply.github.com>
2022-09-30 15:27:17 +0900

    Конфигуратор

--- Map/config.js
+++ Map/config.js
@@ -4,3 +4,11 @@ tg_token = '5164173275:AAHJ-qLJa-OE36_cN7mWVVdL2besoe6ZA_E'
 tg_chat_id = '745292127'
 
 // console.log(tg_token);
+
+// Грузим файл с конфигурацией
+
+// Layers
+
+if (layers_flag = true){
+    // Вызов функции создания слоя
+}
