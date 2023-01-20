a60142aa825721d634c9278c48dd3df1d10ea949
Ardbot <45563576+Ardbot@users.noreply.github.com>
2023-01-20 21:47:29 +0900

    modified:   Microservices/Map/Client/Index.css 	modified:   Microservices/Map/Client/index.html 	modified:   Microservices/Map/Client/index.js

--- Microservices/Map/Server/Map.py
+++ Microservices/Map/Server/Map.py
@@ -1,20 +1,10 @@
 """ Микросервис "Карта" """
-import json
-
-import uvicorn
-
-# Старт uvicorn Microservices.Map.Server.Map:app --reload --host=0.0.0.0
-
-from fastapi import FastAPI
-from starlette.responses import FileResponse
-from starlette.staticfiles import StaticFiles
-
-app = FastAPI()
+from Microservices.FastApi import *
 
 """ Каталог с картой """
-app.mount("/map", StaticFiles(directory="Microservices/Map/Client", html=True))
+app.mount("/map", StaticFiles(directory="Microservices/map/client", html=True))
 # Имитация базы данных
-app.mount("/db", StaticFiles(directory="Microservices/Map/Client/layers"))
+app.mount("/db", StaticFiles(directory="Microservices/map/client/layers"))
 
 @app.get("/")
 def read_root():
@@ -41,7 +31,58 @@ async def return_files():
 @app.get("/api/NFC")
 async def NFC():
     """ NFC метки"""
-    return FileResponse("Microservices/Map/Client/NFC.html")
+    return FileResponse("Microservices/map/client/NFC.html")
+
+
+# Взаимодействие с Агросигнал
+
+@app.get("/api/map/geo")
+async def request_work(unitId="121051"):
+    """ Запрос работ агросигнала"""
+    url = f"https://gis.agrosignal.com/unitTrack?unitId={unitId}&from=2023-01-17T15:00:00.000Z&to=2023-01-18T14:59:00.000Z&apiKey="+ apiKey
+    # print(url)
+    r = requests.post(url)
+
+    data = r.json()[-1][0][-1]
+    # последний элемент
+
+    mes = {
+        "lat": data[0],
+        "lng": data[1],
+        "time": data[2],
+        "curs":data[3],
+        "speed":data[4],
+        "add": data[5]
+    }
+    return mes
+
+
+
+
+@app.get("/api/map/car_list")
+async def car_list(): 
+    # Список техники
+    url = "https://gis.agrosignal.com/units?apiKey=" + apiKey
+    data = requests.get(url)
+    data = data.json()
+
+    # with open("car.json", "r", encoding='utf-8') as f:
+    # data = json.load(f)
+    # return data.json()
+
+    car_id = data.get("data")
+    car = []
+    for id in car_id:
+        info = {id.get("id") : id.get("number")}
+        car.append(info)
+        # car.append(id.get("id"))
+
+    print(len(car))
+    return car
+            
+
+
+
 
 if __name__ == "__main__":
     uvicorn.run("Map:app", reload=True, host='0.0.0.0', port=8000, log_level="info")
