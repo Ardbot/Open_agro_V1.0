b4fe61aa2a2133d441354758292ee635fdd66935
Ardbot <45563576+Ardbot@users.noreply.github.com>
2023-02-02 01:52:43 +0900

    Только микросервисы

--- hardware/ESP8266/thing_temp/secrets.h
+++
@@ -1,8 +0,0 @@
-// Use this file to store all of the private credentials 
-// and connection details
-
-#define SECRET_SSID "ANK"		// replace MySSID with your WiFi network name
-#define SECRET_PASS "08Glah5Z"	// replace MyPassword with your WiFi password
-
-#define SECRET_CH_ID 1384395			// replace 0000000 with your channel number
-#define SECRET_WRITE_APIKEY "ETSGVZ600T5AWTH7"   // replace XYZ with your channel write API Key
