b6729aa098487da96f7d817a0a9608c599b7f1b2
Ardbot <45563576+Ardbot@users.noreply.github.com>
2023-02-03 02:48:23 +0900

    WS

--- tools/webSocket.js
+++ tools/webSocket.js
@@ -6,23 +6,13 @@ log(apiKey)
 let socket = new WebSocket("wss://gis.agrosignal.com/data/play?apiKey=" + apiKey);
 
 // msg = '{"event":"start","cid": "5c849875b7","data":{"from":"2023-02-01T15:00:00.000Z","dynamic":[],"archiveFrom":null,"cid": "5c8498d0bf75b7"}}'
-msg = {
-    "event": "start",
-    "cid": "5c8bf75b7",
-    "data": {
-        // "from": "2023-02-02T15:00:00.000Z",
-        "from": "2023-02-02T16:05:32.900Z",
-        "dynamic": [],
-        "archiveFrom": null,
-        "cid": "5c84975b7"
-    }
-}
 
-msgJson = JSON.stringify(msg)
+
+
 
 socket.onopen = function (e) {
     // Выполняем рукопожатие
-    socket.send(msgJson);
+
 };
 
 socket.onmessage = function (event) {
@@ -47,9 +37,42 @@ socket.onerror = function (error) {
 
 function parseWS(data) {
     ws = JSON.parse(data);
-    // console.log(ws?.data?.items); [1].values.rt_position
-    items = ws?.data?.items;
-    for (id in items) {
-        console.log(items[id].values.rt_position); 
+    if (ws?.event == "changes") {
+        items = ws.data.items;
+        // console.log(items)
+        for (id in items) {
+            var marker1, circle1
+
+            var popup1 = L.popup();
+            position = items[id]?.values?.position
+            if (position != undefined) {
+
+                lat = position[1]
+                long = position[0]
+                log(lat, long)
+
+                marker = L.marker([lat, long])
+                console.log(items[id].id)
+                popup1
+                    .setLatLng([lat, long])
+                    .setContent(String(items[id]?.id));
+                // circle = L.circle([lat, long], 10)
+                var featureGroup = L.featureGroup([marker, popup1]).addTo(map);
+            }
+        }
+    }
+
+    else if (ws?.event == "youAre") {
+        msg = {
+            "event": "start",
+            "cid": ws.data.id,
+            "data": {
+                "from": "2023-03-02T00:00:00.900Z",
+                "dynamic": [],
+                "archiveFrom": null,
+                "cid": ws.data.id
+            }
+        }
+        socket.send(JSON.stringify(msg));
     }
 }
