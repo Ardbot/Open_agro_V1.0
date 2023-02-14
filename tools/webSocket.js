bde56f4890a28ec70164645743a86c861b5eb7ff
Ardbot <45563576+Ardbot@users.noreply.github.com>
2023-02-14 21:13:16 +0900

    modified:   tools/webSocket.js 	modified:   map/client/index.html 	modified:   map/client/navigation.js

--- tools/webSocket.js
+++ tools/webSocket.js
@@ -1,20 +1,21 @@
 //  Устанавливаем соединение с websocket
-// document.cookie = "user=John; secure";
 
-let apiKey = localStorage.getItem('apiKey').slice(1, -1)
-// log(apiKey)
+
+let apiKey = localStorage.getItem("apiKey");
+
 let socket = new WebSocket("wss://gis.agrosignal.com/data/play?apiKey=" + apiKey);
+console.log(socket);
+
+if (apiKey == undefined) {
+    console.log("ApiKey не установлен! Введите в настройках.");
+}
 
 socket.onopen = function (e) {
-
 };
 
+// Обрабатываем ответ
 socket.onmessage = function (event) {
-    // log(typeof (event))
-    // log(event.data)
-    // Обрабатываем ответ
     parseWSagrosignal(event.data);
-
 };
 
 socket.onclose = function (event) {
@@ -33,23 +34,24 @@ socket.onerror = function (error) {
 
 
 let dataAS = { "event": "changes", "data": { "time": 1677715200900, "items": [{ "id": 121253, "time": 1675380523000, "last": 1675380523000, "values": { "rt_time": 1675380523000, "position": [127.50855, 50.2895125] } }, { "id": 121245, "time": 1675380397000, "last": 1675380397000, "values": { "rt_time": 1675380397000, "position": [127.74067, 50.183020000000006] } }, { "id": 121138, "time": 1675380525000, "last": 1675380525000, "values": { "rt_time": 1675380525000, "position": [127.76482333333334, 50.25517333333334] } }, { "id": 121270, "time": 1675380528000, "last": 1675380528000, "values": { "rt_time": 1675380528000, "position": [127.76603916666667, 50.2479125] } }] } }
-// log(typeof (dataAS))
 
 // Соотносит id c номером машины
 function carNumF(asId) {
     carsList = JSON.parse(localStorage.getItem('car_list'))
     for (num in carsList) {
-        if (asId in carsList[num]){
+        if (asId in carsList[num]) {
             return carsList[num][asId]
         }
     }
 }
 
+
+
 // Парсим данные с Агросигнала
 function parseWSagrosignal(dataAS) {
     let ws = JSON.parse(dataAS); // прод
     // ws = dataAS;    // Отладка
-    // console.log(dataAS);
+
     // Блок с данными
     if (ws?.event == "changes") {
         items = ws.data.items;
@@ -59,7 +61,7 @@ function parseWSagrosignal(dataAS) {
             let asTime = items[id].time;
             let asLast = items[id].last;
             let asValues = items[id].values;
-            log(num, new Date(asTime), new Date(asLast), asValues)
+            // log(num, new Date(asTime), new Date(asLast), asValues)
 
             // Обработка местоположения
             let position = asValues?.position
@@ -68,8 +70,7 @@ function parseWSagrosignal(dataAS) {
                 long = position[0]
 
                 addMarker([lat, long], carNum = String(num))
-                // data = {};
-                // carMarker.markers.push([carMarker.markers[asId] = asId, [lat, long]]);
+
             }
             // Позиция
             // Задания
@@ -77,23 +78,11 @@ function parseWSagrosignal(dataAS) {
             // position = items[id]?.values?.position
         }
 
-        // 
-        // addMarker();
-
-        // for (car in carMarker.markers) {
-        //     // addMarker();
-        //     console.log(carMarker.markers[car]);
-        // }
-
-
-        // console.log(carMarker.markers);
-
     }
     // Блок с "рукопожатием"
     else if (ws?.event == "youAre") {
         console.log("Авторизация");
         var now = new Date().toISOString();
-        log(now)
         msg = {
             "event": "start",
             "cid": ws.data.id,
@@ -108,72 +97,6 @@ function parseWSagrosignal(dataAS) {
     }
     else console.log("Не распознано")
 }
-
-// Список маркеров
-// var carMarker = [];
-// var popup1 = L.popup();
-// Добавить маркер
-// function addMarker([lat, long], carNum = '0') {
-
-// Создаем маркер
-// marker = L.marker([lat, long], { title: carNum });
-// markerGroup  Добавить
-// Всплывающее окно
-// var popup = L.popup()
-//     .setLatLng([lat, long])
-//     .setContent(String(carNum));
-
-// Добавляем в список
-// key = carNum;
-
-// title = marker.options.title;
-// lat = marker._latlng.lat;
-// long = marker._latlng.lng;
-
-// data = {
-//     title,
-//     lat,
-//     long
-// }
-
-// carMarker[key] = data;
-// console.log(carMarker);
-
-// var featureGroup = L.featureGroup([markers]).addTo(map);
-
-// }
-
-
-
-
-// }
-
-// // Удаление маркеров
-// function markerDelete(markerId) {
-
-// }
-
-
-// var popup1 = L.popup();
-// var marker1, circle1
-
-// if (position != undefined) {
-
-//     lat = position[1]
-//     long = position[0]
-//     log(lat, long)
-
-//     marker = L.marker([lat, long])
-//     // console.log(items[id].id)
-//     popup1
-//         .setLatLng([lat, long])
-//         .setContent(String(items[id]?.id));
-//     // circle = L.circle([lat, long], 10)
-//     var featureGroup = L.featureGroup([marker, popup1]).addTo(map);
-// }
-
-// парсить
-
 
 document.querySelectorAll('.parseWS').forEach(el => el.addEventListener('click', () => { parseWSagrosignal(dataAS) }));
 document.querySelectorAll('.addMarker').forEach(el => el.addEventListener('click', () => { addMarker([50.01, 128], carNum = '3') }));
